2394a707b6741282d62f296d4f0f7fa7220fe08f
Erik Petersen <contacterikpetersen@gmail.com>
2016-06-17 12:46:54 -0400

    All pieces advance through 9 moves.  Now will work on going backwards

--- templates/src/assets/web/js/scripts.js
+++ templates/src/assets/web/js/scripts.js
@@ -11,7 +11,7 @@ $( function() {
   var row8 = $( ".row-8 li" );
 
   var chessBoard = [ row1, row2, row3, row4, row5, row6, row7, row8 ];
-  console.log( chessBoard[6][4] );
+  // console.log( chessBoard[6][3] );
 
   var forwardClicks = 0;
   $( ".step-forward" ).on( "click", function() {
@@ -21,31 +21,64 @@ $( function() {
 
     switch ( forwardClicks ) {
       case 1:
-        console.log( "Before: " + chessBoard[6][4].textContent );
-        console.log( "Before: " + chessBoard[4][4].textContent );
+        console.log( "Before: " + chessBoard[6][3].textContent );
+        console.log( "Before: " + chessBoard[4][3].textContent );
 
-        // var aChild = element.appendChild(aChild);
-        var display = chessBoard[6][4];
+        var display = chessBoard[6][3];
         console.log( $(display).children().html() );
-        $(chessBoard[4][4]).append( $(display).html() );
-        $(chessBoard[6][4]).html( "" );
+        $(chessBoard[4][3]).append( $(display).html() );
+        $(chessBoard[6][3]).html( "" );
 
-        // chessBoard[4][4] = chessBoard[6][4];
-        // chessBoard[6][4] = "";
-
-        // $( chessBoard[4][3] ).text( "" );
-        // $( chessBoard[6][3] ).text( $(chessBoard[4][3]) );
-
-        // $( chessBoard[4][4] ).append( $(chessBoard[6][4]) );
-        // $( chessBoard[6][4] ).text( "" );
-
-        console.log( "After: " + chessBoard[6][4].textContent );
-        console.log( "After: " + chessBoard[4][4].textContent );
+        console.log( "After: " + chessBoard[6][3].textContent );
+        console.log( "After: " + chessBoard[4][3].textContent );
         break;
       case 2:
-        console.log( chessBoard[6][3].textContent );
-        chessBoard[4][3].textContent = chessBoard[6][3].textContent;
-        chessBoard[6][3] = "";
+        var display = chessBoard[0][6];
+        console.log( $(display).children().html() );
+        $(chessBoard[2][5]).append( $(display).html() );
+        $(chessBoard[0][6]).html( "" );
+        break;
+      case 3:
+        var display = chessBoard[6][2];
+        console.log( $(display).children().html() );
+        $(chessBoard[4][2]).append( $(display).html() );
+        $(chessBoard[6][2]).html( "" );
+        break;
+      case 4:
+        var display = chessBoard[1][4];
+        console.log( $(display).children().html() );
+        $(chessBoard[2][4]).append( $(display).html() );
+        $(chessBoard[1][4]).html( "" );
+        break;
+      case 5:
+        var display = chessBoard[6][6];
+        console.log( $(display).children().html() );
+        $(chessBoard[5][6]).append( $(display).html() );
+        $(chessBoard[6][6]).html( "" );
+        break;
+      case 6:
+        var display = chessBoard[1][3];
+        console.log( $(display).children().html() );
+        $(chessBoard[3][3]).append( $(display).html() );
+        $(chessBoard[1][3]).html( "" );
+        break;
+      case 7:
+        var display = chessBoard[7][5];
+        console.log( $(display).children().html() );
+        $(chessBoard[6][6]).append( $(display).html() );
+        $(chessBoard[7][5]).html( "" );
+        break;
+      case 8:
+        var display = chessBoard[0][5];
+        console.log( $(display).children().html() );
+        $(chessBoard[1][4]).append( $(display).html() );
+        $(chessBoard[0][5]).html( "" );
+        break;
+      case 9:
+        var display = chessBoard[7][6];
+        console.log( $(display).children().html() );
+        $(chessBoard[5][5]).append( $(display).html() );
+        $(chessBoard[7][6]).html( "" );
         break;
       default:
         break;
